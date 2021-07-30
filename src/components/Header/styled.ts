e78228a17fc018a14e5c7b1baf560465d7afffeb
Pah Ferreira <pah.webdev@gmail.com>
2021-07-30 11:00:12 -0300

    feat: :sparkles: Header Component

---
+++ src/components/Header/styled.ts
@@ -0,0 +1,21 @@
+// Libraries
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  padding: 24px 56px;
+  background-color: ${({ theme }) => theme.colors.white};
+  @media screen and (max-width: 450px) {
+    padding: 16px;
+  }
+`;
+
+export const Logo = styled.img`
+  width: 100px;
+  height: 32px;
+  @media screen and (max-width: 450px) {
+    width: 75px;
+    height: 24px;
+  }
+`;
