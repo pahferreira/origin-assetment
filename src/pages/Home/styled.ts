f4693975a52f55fcd5049e86871941c1fbdba027
Pah Ferreira <pah.webdev@gmail.com>
2021-07-31 23:40:55 -0300

    feat: :sparkles: Homepage

---
+++ src/pages/Home/styled.ts
@@ -0,0 +1,10 @@
+// Libraries
+import styled from 'styled-components';
+
+export const Main = styled.div`
+  min-height: calc(100vh - 80px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
